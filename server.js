79b8f57a921870eb88e9cafcc0bccdf8c158a196
Scott Wagner <af17317596@gmail.com>
2019-04-18 12:11:38 -0500

    Update server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var apiRoutes = require('./app/routing/apiRoutes.js')
+var htmlRoutes = require('./app/routing/htmlRoutes')
+
+// ---- Setting PORT ------ //
+var PORT = process.env.PORT || 3001;
+
+// ---- Ussing Express ---- //
+var app = express();
+
+//---- Using static folder -----//
+app.use(express.static('public'));
+
+//---- Ussing body-parser ----//
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// --- Declaring Routes -----//
+app.use(apiRoutes);
+app.use(htmlRoutes);
+
+// --- Start Server (express)----//
+app.listen(PORT, function() {
+  console.log('App now listening at localhost: ' + PORT);
+});
